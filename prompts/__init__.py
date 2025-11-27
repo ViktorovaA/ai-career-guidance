5ca09ac6dbc5306a539306054afbee1d859f78af
Anastasia Viktorova <viktorovaa.04@gmail.com>
2025-11-27 03:12:54 +0300

    add recomendation

--- prompts/__init__.py
+++ prompts/__init__.py
@@ -3,7 +3,8 @@ from .learning import LEARNING_PROMPT
 from .riasec import RIASEC_PROMPT
 from .skills import SKILLS_PROMPT
 from .values import VALUES_PROMPT
+from .recommendations import RECOMMENDATIONS_PROMPT
 
-__all__ = ['RIASEC_PROMPT', 'SKILLS_PROMPT', 'VALUES_PROMPT', 'BIG5_PROMPT', 'LEARNING_PROMPT']
+__all__ = ['RIASEC_PROMPT', 'SKILLS_PROMPT', 'VALUES_PROMPT', 'BIG5_PROMPT', 'LEARNING_PROMPT', 'RECOMMENDATIONS_PROMPT']
 
 
